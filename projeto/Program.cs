eec194551ffcf75e94d038a0db13aedf7978e6b8
Joao Pedro Caixeta <j.p.a.caixeta@gmail.com>
2016-09-26 12:21:17 -0300

    comecando a implementar o tabuleiro

--- projeto/Program.cs
+++ projeto/Program.cs
@@ -5,10 +5,7 @@ namespace projetoxadrez {
     class Program {
         static void Main(string[] args) {
 
-            Posicao p;
-            p = new Posicao(1,2);
-
-            Console.WriteLine(p);
+            Tabuleiro tab = new Tabuleiro(8, 8);
 
             Console.ReadLine();
         }
--- projeto/tabuleiro/tabuleiro.cs
+++ projeto/tabuleiro/tabuleiro.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace tabuleiro{
     class Tabuleiro {
-        
 
+        private Peca[,] pecas;
+        public int linhas { get; set; }
+        public int colunas { get; set; }
+
+        public Tabuleiro(int linhas, int colunas) {
+            this.linhas = linhas;
+            this.colunas = colunas;
+            pecas = new Peca[linhas, colunas];
+        }
     }
 }
