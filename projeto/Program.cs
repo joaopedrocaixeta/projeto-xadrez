f68e6897933da1aaf230bc98a141a4e4a3c07606
Joao Pedro Caixeta <j.p.a.caixeta@gmail.com>
2016-09-26 09:21:41 -0300

    Primeiro commit

---
+++ projeto/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um vslor intereiro");
+
+            int x = int.Parse(Console.ReadLine());
+
+            int y = 2 * x;
+
+            Console.WriteLine("Dobro = " + y);
+
+            Console.ReadLine();
+
+        }
+    }
+}
